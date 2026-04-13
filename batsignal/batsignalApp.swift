ba2f6582f07658725a7dd6020b7a00409f7c8d6a
Aiden Drugge <aidendrugge@gmail.com>
2026-04-12 22:53:39 -0700

    Initial Commit

---
+++ batsignal/batsignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  batsignalApp.swift
+//  batsignal
+//
+//  Created by Aiden Drugge on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct batsignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
